77cc8c81888db6362ed9e2c0748fea4b19906dd7
Tony.Hansen <tony.hansen@pioneer.com>
2017-10-12 11:59:28 -0500

    Test data push and prod zip file

--- _DEV/stats.php
+++ _DEV/stats.php
@@ -45,119 +45,72 @@
 					<div class="row">
 						<div class="12u">
 							<section id="content" >
-								<header>
-									<h2>Player Stats</h2>
-								</header>
-								<div id="standings">
-								<table>
-									<thead>
-										<tr>
-											<th>Team</th>
-											<th>GP</th>
-											<th>Wins</th>
-											<th>Losses</th>
-											<th>SOL</th>
-											<th>TIE</th>
-											<th>Pts</th>
-											<th>GF</th>
-											<th>GA</th>
-										</tr>
-									</thead>
-									<tbody>
+							
 
+								<div id="stats">
+								<h3>Goon List</h3>
+								<hr />
+								<p><em><font size=-1>(Players on the "Goon List" are those who average over a penalty minute per game.)</font></em></p>
+								<table id="goon" align="center">
+								<thead>
+									<tr>
+										<th align=left>First</th>
+										<th align=left>Last</th>
+										<th align=left>Team</th>
+										<th>PM</th>
+										<th>PM/G</th>
+									</tr>
+								</thead>
+								<tbody>
 <?php
 	include ("common/db_setup.php");
-	$query = "SELECT t.name, 
-				(select count(*) from game 
-					join schedule on game.id = schedule.id 
-					where schedule.home=t.id or schedule.away=t.id) as 'GP', 
-				(select count(*) from game as g 
-					join schedule as s on g.id = s.id 
-					where (t.id = s.home and g.home_score > g.away_score) 
-						or (t.id = s.away and g.home_score < g. away_score) 
-						or (t.id = s.home and g.result = 2) 
-						or (t.id = s.away and g.result = 1)
-						or (t.id = s.home and g.result = 4)
-						or (t.id = s.away and g.result = 3)) as 'W', 
-				(select count(*) from game as g 
-					join schedule as s on g.id = s.id 
-					where (t.id = s.home and g.home_score < g. away_score) 
-						or (t.id = s.away and g.home_score > g. away_score)
-						or (t.id = s.home and g.result = 3)
-						or (t.id = s.away and g.result = 4)) as 'L', 
-				(select count(*) from game as g 
-					join schedule as s on g.id = s.id 
-					where (t.id = s.away and g.result = 2) 
-						or (t.id = s.home and g.result = 1)) as 'SOL', 
-				(select count(*) from game as g 
-					join schedule as s on g.id = s.id 
-					where ((t.id = s.away or t.id = s.home) and g.result = 5)) as 'TIE', 
-				((select count(*) from game as g 
-					join schedule as s on g.id = s.id 
-					where (t.id = s.home and g.home_score > g. away_score) 
-					or (t.id = s.away and g.home_score < g. away_score) 
-					or (t.id = s.home and g.result = 2) 
-					or (t.id = s.away and g.result = 1)
-					or (t.id = s.home and g.result = 4)
-					or (t.id = s.home and g.result = 3))*2) + 
-						(select count(*) from game as g 
-							join schedule as s on g.id = s.id 
-							where (t.id = s.away and g.result = 2) 
-								or (t.id = s.home and g.result = 1)
-								or ((t.id = s.away or t.id = s.home) and g.result = 5)) as 'Pts',
-				(select sum(home_score) from game g
-					join schedule s on g.id = s.id
-					where t.id = s.home) +
-				(select sum(away_score) from game g
-					join schedule s on g.id = s.id
-					where t.id = s.away) as 'GF',
-				(select sum(away_score) from game g
-					join schedule s on g.id = s.id
-					where t.id = s.home) +
-				(select sum(home_score) from game g
-					join schedule s on g.id = s.id
-					where t.id = s.away) as 'GA'
-				from team as t 
-				where season = 9
-				order by 7 desc, 3 desc, 4 desc, 8 desc, 1";
-				
+	$connection = mysqli_connect($server, $username, $password, $database) or die ("Connection failed");
+		$query = "SELECT pr.first, pr.last, t.name as 'team', sum(p.duration) as pm, ROUND(sum(p.duration)/gp.games, 2 ) AS  'pm_per_game'
+		FROM team t
+		JOIN player pl
+			ON t.id = pl.team
+		JOIN person pr
+			ON pl.person = pr.id
+		JOIN event e
+			ON pl.id = e.player
+		JOIN penalty p
+			ON e.id = p.id
+		JOIN (SELECT t.id, count(t.id) as 'games'
+		        FROM team t
+		        JOIN schedule s
+		            ON t.id = s.home or t.id = s.away
+		        JOIN game g
+		            ON s.id = g.id
+				GROUP BY t.id) gp
+		ON t.id = gp.id
+		WHERE t.season = 9
+			AND e.type = 3
+		GROUP BY pr.first, pr.last, t.name
+		HAVING pm_per_game > 1
+		ORDER BY pm DESC, pr.last, pr.first";
+
 	$result = mysqli_query($connection, $query) or die("Query failed");
 	while ($row = mysqli_fetch_assoc($result)) {
-		$team = $row['name'];
-		$games = $row['GP'];
-		$wins = $row['W'];
-		$losses = $row['L'];
-		$sol = $row['SOL'];
-		$tie = $row['TIE'];
-		$points = $row['Pts'];
-		$gf = $row['GF'];
-		$ga = $row['GA'];
-		
+		$first = $row['first'];
+		$last = $row['last'];
+		$team = $row['team'];
+		$pen_min = $row['pm'];
+		$pm_per_game = $row['pm_per_game'];
 		echo "<tr><td align=left><strong>";
+		echo htmlentities($first);
+		echo "</strong></td><td align=left><strong>";
+		echo htmlentities($last);
+		echo "</strong></td><td align=left>";
 		echo htmlentities($team);
-		echo "</strong></td><td align=center>";
-		echo htmlentities($games);
-		echo "</td><td align=center>";
-		echo htmlentities($wins);
-		echo "</td><td align=center>";
-		echo htmlentities($losses);
-		echo "</td><td align=center>";
-		echo htmlentities($sol);
-		echo "</td><td align=center>";
-		echo htmlentities($tie);
 		echo "</td><td align=center><strong>";
-		echo htmlentities($points);
+		echo htmlentities($pen_min);
 		echo "</strong></td><td align=center>";
-		echo htmlentities($gf);
-		echo "</td><td align=center>";
-		echo htmlentities($ga);
+		echo htmlentities($pm_per_game);
 		echo "</td></tr>";
 	}
 	mysqli_free_result($result);
 	mysqli_close($connection);
 ?>
-
-
 </tbody>
 </table>
 </div>
---
+++ _TEST/standings.php
@@ -0,0 +1,169 @@
+<!DOCTYPE HTML>
+<!--
+	Synchronous by TEMPLATED
+    templated.co @templatedco
+    Released for free under the Creative Commons Attribution 3.0 license (templated.co/license)
+-->
+<html>
+	<head>
+		<title>DMAHockey - Standings</title>
+		<meta http-equiv="content-type" content="text/html; charset=utf-8" />
+		<meta name="description" content="" />
+		<meta name="keywords" content="" />
+		<link href='http://fonts.googleapis.com/css?family=Lato:300,400,700,900' rel='stylesheet' type='text/css'>
+		<!--[if lte IE 8]><script src="js/html5shiv.js"></script><![endif]-->
+		<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
+		<script src="js/skel.min.js"></script>
+		<script src="js/skel-panels.min.js"></script>
+		<script src="js/init.js"></script>
+		<noscript>
+			<link rel="stylesheet" href="css/skel-noscript.css" />
+			<link rel="stylesheet" href="css/style.css" />
+		</noscript>
+		<!--[if lte IE 8]><link rel="stylesheet" href="css/ie/v8.css" /><![endif]-->
+		<!--[if lte IE 9]><link rel="stylesheet" href="css/ie/v9.css" /><![endif]-->
+	</head>
+	<body>
+		<div id="wrapper">
+			
+			<!-- Header -->
+			<?php include ("common/header.php");?>
+			<!-- /Header -->
+			
+			<div id="page">
+				<div class="container">
+					<div class="row">
+						<div class="12u">
+							<section id="content" >
+								<header>
+									<h2>2017-18 Team Standings</h2>
+								</header>
+								<div id="standings">
+								<table>
+									<thead>
+										<tr>
+											<th>Team</th>
+											<th>GP</th>
+											<th>Wins</th>
+											<th>Losses</th>
+											<th>SOL</th>
+											<th>TIE</th>
+											<th>Pts</th>
+											<th>GF</th>
+											<th>GA</th>
+										</tr>
+									</thead>
+									<tbody>
+
+<?php
+	include ("common/db_setup.php");
+	$query = "select t.name, 
+				(select count(*) from game 
+					join schedule on game.id = schedule.id 
+					where schedule.home=t.id or schedule.away=t.id) as 'GP', 
+				(select count(*) from game as g 
+					join schedule as s on g.id = s.id 
+					where (t.id = s.home and g.home_score > g.away_score) 
+						or (t.id = s.away and g.home_score < g. away_score) 
+						or (t.id = s.home and g.result = 2) 
+						or (t.id = s.away and g.result = 1)
+						or (t.id = s.home and g.result = 4)
+						or (t.id = s.away and g.result = 3)) as 'W', 
+				(select count(*) from game as g 
+					join schedule as s on g.id = s.id 
+					where (t.id = s.home and g.home_score < g. away_score) 
+						or (t.id = s.away and g.home_score > g. away_score)
+						or (t.id = s.home and g.result = 3)
+						or (t.id = s.away and g.result = 4)) as 'L', 
+				(select count(*) from game as g 
+					join schedule as s on g.id = s.id 
+					where (t.id = s.away and g.result = 2) 
+						or (t.id = s.home and g.result = 1)) as 'SOL', 
+				(select count(*) from game as g 
+					join schedule as s on g.id = s.id 
+					where ((t.id = s.away or t.id = s.home) and g.result = 5)) as 'TIE', 
+				((select count(*) from game as g 
+					join schedule as s on g.id = s.id 
+					where (t.id = s.home and g.home_score > g. away_score) 
+					or (t.id = s.away and g.home_score < g. away_score) 
+					or (t.id = s.home and g.result = 2) 
+					or (t.id = s.away and g.result = 1)
+					or (t.id = s.home and g.result = 4)
+					or (t.id = s.home and g.result = 3))*2) + 
+						(select count(*) from game as g 
+							join schedule as s on g.id = s.id 
+							where (t.id = s.away and g.result = 2) 
+								or (t.id = s.home and g.result = 1)
+								or ((t.id = s.away or t.id = s.home) and g.result = 5)) as 'Pts',
+				(select sum(home_score) from game g
+					join schedule s on g.id = s.id
+					where t.id = s.home) +
+				(select sum(away_score) from game g
+					join schedule s on g.id = s.id
+					where t.id = s.away) as 'GF',
+				(select sum(away_score) from game g
+					join schedule s on g.id = s.id
+					where t.id = s.home) +
+				(select sum(home_score) from game g
+					join schedule s on g.id = s.id
+					where t.id = s.away) as 'GA'
+				from team as t 
+				where season = 9
+				order by 7 desc, 3 desc, 4 desc, 8 desc, 1";
+				
+	$result = mysqli_query($connection, $query) or die("Query failed");
+	while ($row = mysqli_fetch_assoc($result)) {
+		$team = $row['name'];
+		$games = $row['GP'];
+		$wins = $row['W'];
+		$losses = $row['L'];
+		$sol = $row['SOL'];
+		$tie = $row['TIE'];
+		$points = $row['Pts'];
+		$gf = $row['GF'];
+		$ga = $row['GA'];
+		
+		echo "<tr><td align=left><strong>";
+		echo htmlentities($team);
+		echo "</strong></td><td align=center>";
+		echo htmlentities($games);
+		echo "</td><td align=center>";
+		echo htmlentities($wins);
+		echo "</td><td align=center>";
+		echo htmlentities($losses);
+		echo "</td><td align=center>";
+		echo htmlentities($sol);
+		echo "</td><td align=center>";
+		echo htmlentities($tie);
+		echo "</td><td align=center><strong>";
+		echo htmlentities($points);
+		echo "</strong></td><td align=center>";
+		echo htmlentities($gf);
+		echo "</td><td align=center>";
+		echo htmlentities($ga);
+		echo "</td></tr>";
+	}
+	mysqli_free_result($result);
+	mysqli_close($connection);
+?>
+
+
+</tbody>
+</table>
+</div>
+
+
+								</section>
+						</div>
+					</div>
+
+				</div>	
+			</div>
+
+
+			<!-- Footer -->
+			<?php include ("common/footer.php");?>
+			
+		</div>
+	</body>
+</html>
